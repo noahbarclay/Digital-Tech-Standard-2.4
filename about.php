90eaaeedbd2a73833ebe4104e4be6131ccc2477d
noahbarclay <nb56@mahurangi.school.nz>
2021-08-23 10:10:24 +1200

    transfer to pc

--- about.php
+++ about.php
@@ -2,5 +2,19 @@
 
 <p>About</p>
 
+<button onclick="myFunction()">Try it</button>
+
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+  var person = prompt("Please enter your name", "Enter Here");
+  if (person != null) {
+    document.getElementById("demo").innerHTML =
+    "Hello " + person + "! How are you today?";
+  }
+}
+</script>
+
 <?php include("footer.html");?>
 
