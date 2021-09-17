7b8cf94ba2f776cbb5f5b3f91f86523598fc3f44
noahbarclay <nb56@mahurangi.school.nz>
2021-09-18 10:43:17 +1200

    Scanning Dates section

--- index.php
+++ index.php
@@ -30,4 +30,41 @@
 
   <img src="http://localhost/L2%20web/Digital-Tech-Standard-2.4/images/crop.jpg" alt="Scanning on machine" style="width:102%;">
 
+  <!-- The Booking Section -->
+  <div class="w3-black" id="tour">
+    <div class="w3-container w3-content w3-padding-64" style="max-width:1500px;background-color:black">
+      <h2 style="text-size:100px" class="w3-wide w3-center w3-padding">SCANNING DATES</h2>
+      <div class="w3-row-padding w3-padding-32" style="margin:0 0px">
+        <div class="w3-third w3-margin-bottom">
+          <img src="http://localhost/L2%20web/Digital-Tech-Standard-2.4/images/optimized/f45.jpg" alt="F45" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>F45 Fitness</b></p>
+            <p class="w3-opacity">Fri 27 Nov 2021</p>
+            <p>F45 scans as part of challenge pack, only verified members.</p>
+            <span class="w3-tag w3-red w3-margin-left">Sold out</span></li>
+          </div>
+        </div>
+        <div class="w3-third w3-margin-bottom">
+          <img src="http://localhost/L2%20web/Digital-Tech-Standard-2.4/images/optimized/anytime.jpg" alt="Anytime" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>Anytime Fitness</b></p>
+            <p class="w3-opacity">Sat 5 Dec 2021</p>
+            <p>Cactch up scans for members who missed first scans.</p>
+            <span class="w3-tag w3-red w3-margin-left">Sold out</span></li>
+          </div>
+        </div>
+        <div class="w3-third w3-margin-bottom">
+          <img src="http://localhost/L2%20web/Digital-Tech-Standard-2.4/images/optimized/9round.png" alt="9round" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>9Round</b></p>
+            <p class="w3-opacity">Fri 11 Nov 2021</p>
+            <p>Only scans as part of the Keep Fit challenge over lockdown.</p>
+            <button style="width: 100px" class="btn btn-success" onclick=" window.open('http://vitalbodyscan.co.nz/book-a-scan.php','_blank')"> Book Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
 <?php include("footer.html");?>
