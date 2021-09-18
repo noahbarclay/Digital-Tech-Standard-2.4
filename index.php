b967b38eb9b8c42c5d7318e62a0b1830dac15ded
noahbarclay <nb56@mahurangi.school.nz>
2021-09-18 12:26:38 +1200

    page info paragraphs

--- index.php
+++ index.php
@@ -30,9 +30,78 @@
 
   <img src="http://localhost/L2%20web/Digital-Tech-Standard-2.4/images/crop.jpg" alt="Scanning on machine" style="width:102%;">
 
+
+
+  <!-- About Section -->
+ <div style="max-width: 1400px; margin:0 auto;"> 
+  <div class="w3-row w3-padding-64" id="about">
+    <div class="w3-col m6 w3-padding-large">
+     <img src="http://localhost/L2%20web/Digital-Tech-Standard-2.4/images/optimized/portrait.jpg" class="w3-round w3-image w3-opacity-min" alt="Machine 1" width="500" height="550">
+    </div>
+
+    <div class="w3-col m6 w3-padding-large">
+      <h1>About Vital Body Scan</h1><br>
+      <h5 class="w3-center"></h5>
+      <p class="w3-large">Vital Body Scan NZ provides the Whangarei &Auckland Region with the latest technology in Body Composition Analysis. 
+
+        Our mission is to help you track your health and fitness goals in the most accurate way possible.
+        
+        We do this by using patented medically graded analysers that provide you with Vital information that is needed to correctly monitor and track your progress.
+        
+        Our analysers are different to commonly used testing devices available in New Zealand as it does not utilise estimations and averages instead it uses patented direct, segmental, multi-frequency, bio-electrical impedance analysis. 
+        
+        This means each individual can be accurately assessed on their own body composition of the trunk, arms, and legs of their body. This then provides an incredibly accurate, repeatable analysis of the overall body composition and health status.
+        
+      </p>
+      <p class="w3-large w3-text-grey w3-hide-medium">Additionally, this Inbody technology has been scientifically verified to be 98.4% as accurate as a DEXA scan ( dual-energy X-Ray absorptiometry) without any exposure to radiation and providing more comprehensive and unique data.</p>
+    </div>
+  </div>
+  
+  <hr>
+
+  <div class="w3-row w3-padding-64" id="about">
+    <div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center">Scanning Locations</h1><br>
+      <h5 class="w3-center">Operating within the Auckland region</h5><br>
+      <p class="w3-large">PENROSE OFFICE - Bookings Essential 
+
+        <class class="w3-center">
+
+        Tuesday 25th May 
+        
+        Thursday 27th May
+        
+        Monday 31st May
+        
+        Thursday 3rd June
+        
+        Friday 4th June
+        
+        Monday 7th June (Queens Birthday)
+        
+        Click below to book
+        
+        <a href="url">https://calendly.com/info-1971</a> 
+        
+        Outside of listed times, please contact us info@vitalbodyscan.co.nz to organise an appointment.
+        
+        MOBILE SESSIONS 
+
+        Monday 14th June 5.30pm-6.30pm Anytime Whangaparaoa</p>
+
+    </div>
+
+    <div class="w3-col m6 w3-padding-large">
+      <img src="http://localhost/L2%20web/Digital-Tech-Standard-2.4/images/optimized/machine.jpg" class="w3-round w3-image w3-opacity-min" alt="Machine 2" width="600" height="750">
+     </div>
+  </div>
+</div>
+
+
   <!-- The Booking Section -->
+
   <div class="w3-black" id="tour">
-    <div class="w3-container w3-content w3-padding-64" style="max-width:1500px;background-color:black">
+    <div class="w3-container w3-padding-64" style="max-width:1700px;background-color:black">
       <h2 style="text-size:100px" class="w3-wide w3-center w3-padding">SCANNING DATES</h2>
       <div class="w3-row-padding w3-padding-32" style="margin:0 0px">
         <div class="w3-third w3-margin-bottom">
@@ -58,13 +127,13 @@
           <div class="w3-container w3-white">
             <p><b>9Round</b></p>
             <p class="w3-opacity">Fri 11 Nov 2021</p>
-            <p>Only scans as part of the Keep Fit challenge over lockdown.</p>
+            <p>Scans as part of the Keep Fit challenge over lockdown.</p>
             <button style="width: 100px" class="btn btn-success" onclick=" window.open('http://vitalbodyscan.co.nz/book-a-scan.php','_blank')"> Book Now</button>
           </div>
         </div>
       </div>
     </div>
   </div>
-
+  
 
 <?php include("footer.html");?>
