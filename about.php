49b090c8a241a14726e9cd0496d97d0587ae991e
noahbarclay <nb56@mahurangi.school.nz>
2021-09-18 14:08:31 +1200

    about us page

--- about.php
+++ about.php
@@ -1,6 +1,31 @@
 <?php include("head-nav.html");?>
 
-<p>About</p>
+<!-- Container (About Section) -->
+<div class="w3-content w3-container w3-padding-64" id="about">
+  <h3 style="width:800px; margin:0 auto; padding-bottom: 20px"">ABOUT US</h3>
+  <p style="width:800px; margin:0 auto; padding-bottom: 20px">​I love to read about peoples personal stories on their journey to better health so thought it was about time I shared a little of my own and why we started Vital Body Scan NZ.
+
+My own journey has seen me go from 87 kilos to 62 kilos. What I have learned is that it's not about that magic number on the scales anymore! 
+
+In 2014 we went to live in Australia for the year and came across a business called Rapid Bioscan, check them out if you are in Australia http://www.rapidbioscan.com.au/. They offered comprehensive reports on body composition so that you can look beyond your weight on the scale and track the numbers that really matter. </p>
+  <div class="w3-row">
+    <div class="w3-col m6 w3-center w3-padding-large">
+      <p><b><i class="fa fa-user w3-margin-right"></i>Deej and Tasha Barclay</b></p><br>
+      <img src="http://localhost/L2%20web/Digital-Tech-Standard-2.4/images/20191102_173101.jpg" class="w3-round w3-image" alt="Photo of Me" width="500" height="333">
+    </div>
+
+    <!-- Hide this text on small devices -->
+    <div class="w3-col m6 w3-hide-small w3-padding-epic" style="padding: 70px 0px 100px 0px">
+      <p>To be completely honest, when I was first scanned it was a shock to the system. Having already used other BIA technology, I had a fair idea of where I was sitting but was unaware of how inaccurate these can actually be.
+Having already lost 18kg I wasn’t feeling my heaviest at 69.5kg but could visibly see that there was a lot of room for improvement.  After I had my first scan I could see that I was still sitting at 35.7% body fat.  Instead of focusing on the negative (the dreaded body fat!) they were able to show me that I had a good amount of muscle mass and there were other positive numbers that ticked all the boxes.</p>
+    </div>
+  </div>
+
+  <p style="width:800px; margin:0 auto; padding-bottom:20px"> Our transformation:</P>
+  <img src="http://localhost/L2%20web/Digital-Tech-Standard-2.4/images/better.jpg" class="w3-round w3-image" alt="Photo of Me" width="100%" height="100%">
+
+ 
+
 
 <?php include("footer.html");?>
 
